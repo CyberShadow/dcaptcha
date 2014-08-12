68df6ab0c3bf9fb3e065878e44e1e8b754c75838
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-08-12 21:30:57 +0000

    markov: Fix infinite chains

--- markov.d
+++ markov.d
@@ -26,8 +26,11 @@ template MarkovChain(int LENGTH)
 
 		string[][string[]] dic;
 		foreach (paragraph; paragraphs)
+		{
 			foreach (int n; 0..cast(int)paragraph.length)
 				dic[paragraph[max(n-LENGTH, 0)..n]] ~= paragraph[n];
+			dic[paragraph[max(cast(int)$-LENGTH, 0)..$]] ~= null;
+		}
 		dictionary = dic.assumeUnique();
 	}
 
@@ -37,7 +40,10 @@ template MarkovChain(int LENGTH)
 		immutable(string[])* p;
 		while ((p = tail in dictionary) !is null)
 		{
-			result ~= (*p).sample;
+			auto next = (*p).sample;
+			if (next is null)
+				break;
+			result ~= next;
 			tail = result[max(cast(int)$-LENGTH, 0)..$];
 		}
 		return result;
